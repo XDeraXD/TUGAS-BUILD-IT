9b4c34cbf70520d5dd4f9e9b88eae397c9f1eeed
Dera <dewangakangedewiraa@gmail.com>
2024-09-11 21:59:43 +0800

    first commit

---
+++ SOAL1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+    
+    scanf("%d", &N);
+    
+    int nilai[N];
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &nilai[i]);
+    }
+
+    bubbleSort(nilai, N);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d\n", nilai[i]);
+    }
+
+    return 0;
+}
